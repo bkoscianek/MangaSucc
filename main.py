f69995f0c56c30833e347729a59774ce21dc058c
bkoscianek <bkoscianek@gmail.com>
2018-11-29 22:37:57 +0100

    Revert "Revert "fix before gui implementation""
    This reverts commit b63ed68d8a3e307cbcc0f130d7bdb2f65270a9e9.

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-import web_func as wf
 import base_func as bf
+import web_func as wf
+import PySimpleGUI as sg
 
+# rows = []
+# rows.append([sg.Text("Enter manga name:")])
+# rows.append([sg.Input(do_not_clear=True), sg.OK()])
+#
+# layout = rows
+#
+# app = sg.Window('example').Layout(layout)
+#
+# while True:
+#     event, val = app.Read()
+#     if event is None:
+#         break
+#     print(val)
+#
+# app.close()
 
-
-bf.get_to_dnl_folder()
 manga = wf.search_for_manga()
-chapters = wf.select_chapters(manga)
+chapters = wf.get_chapters(manga)
+chapters = wf.get_list_of_chapters(chapters, 'first', 'last')
 wf.download_chapters(chapters)
