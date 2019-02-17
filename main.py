9d301ab852829c39fd49090cc4bb7cf43e63d50d
Bartosz Kościanek <bkoscianek@gmail.com>
2019-02-17 22:24:37 +0100

    v19.2

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import web_func as wf
 
 rows = []
 rows.append([sg.Text("Enter manga name:")])
-rows.append([sg.Input(do_not_clear=True, key='_MANGA_NAME_'), sg.Button('SEARCH')])
+rows.append([sg.Input(do_not_clear=True, key='_MANGA_NAME_', size=(50, 1)), sg.Button('SEARCH')])
 rows.append([])
 rows.append([sg.Text("Select your manga:")])
 rows.append(
-	[sg.Listbox(values=[], size=(30, 5), key='_MANGAS_', select_mode=sg.SELECT_MODE_SINGLE), sg.Button('NEXT')])
+	[sg.Listbox(values=[], size=(80, 6), key='_MANGAS_', select_mode=sg.SELECT_MODE_SINGLE), sg.Button('NEXT')])
 rows.append([sg.Text('Select a single chapter or first and last chapter that you want to download:')])
 rows.append(
-	[sg.Listbox(values=[], size=(30, 5), key='_CHAPTERS_', select_mode=sg.SELECT_MODE_EXTENDED), sg.Button('DOWNLOAD')])
+	[sg.Listbox(values=[], size=(80, 6), key='_CHAPTERS_', select_mode=sg.SELECT_MODE_EXTENDED), sg.Button('DOWNLOAD')])
 
 layout = rows
 
@@ -38,8 +38,13 @@ while True:
 		app.FindElement('_CHAPTERS_').Update(values=list(chapters.keys()))
 		app.Refresh()
 	elif event == 'DOWNLOAD':
-		first_chapter = values['_CHAPTERS_'][1]
-		last_chapter = values['_CHAPTERS_'][0]
+		if len(values['_CHAPTERS_']) == 1:
+			first_chapter = values['_CHAPTERS_'][0]
+			last_chapter = values['_CHAPTERS_'][0]
+		else:
+			first_chapter = values['_CHAPTERS_'][1]
+			last_chapter = values['_CHAPTERS_'][0]
+
 		chapters_to_dnl = wf.get_list_of_chapters(chapters, first_chapter, last_chapter)
 
 		manga_choice = str(choice)
